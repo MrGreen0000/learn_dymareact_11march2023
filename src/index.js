2d726154803c74246482b993465cb8575d966bc9
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-03-11 16:38:38 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
-// import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const element = (
-  <div>
-    <h1>Bonjour de Dyma</h1>
-    <h2>
-      Il est exactement {new Date().toLocaleTimeString()} lors de l'exécution.
-    </h2>
-  </div>
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-setInterval(() => root.render(element
-
-
-
-
-
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-), 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
