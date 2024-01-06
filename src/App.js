82ec0118b3dc05bc9ee266fb8382a516f4b74c50
Naji Mouflih <91195806+MrGreen0000@users.noreply.github.com>
2024-01-06 17:00:24 +0100

    useEffect() en détail

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import { useEffect, useState } from "react";
 
-function App() {
+function Timer() {
   const [count, setCount] = useState(0);
-  const [title, setTitle] = useState("Dyma");
-  console.log("render App");
 
   useEffect(() => {
-    console.log("effectApp");
-    document.title = title;
-  }, [title]);
+    const id = setInterval(() => {
+      setCount((c) => c + 1);
+    }, 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
 
-  function handleChange(e) {
-    setTitle(e.target.value);
-  }
+  return <p>{count}</p>;
+}
 
-  return (
-    <>
-      <h1>Hello world !</h1>
-      <input
-        type="text"
-        onChange={handleChange}
-        value={title}
-        className="mr-15"
-      />
-      <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
-        {count}
-      </button>
-    </>
-  );
+function App() {
+  return <Timer />;
 }
 
 export default App;
