7a7fb9fd3366df5b660586832fdbdfa4255ad16d
Naji Mouflih <91195806+MrGreen0000@users.noreply.github.com>
2024-01-06 17:48:05 +0100

    useLayoutEffect() et le DOM

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import { useEffect, useState } from "react";
-
-function Timer() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const id = setInterval(() => {
-      setCount((c) => c + 1);
-    }, 1000);
-    return () => {
-      clearInterval(id);
-    };
-  }, []);
-
-  return <p>{count}</p>;
-}
+import { useLayoutEffect, useRef, useState } from "react";
 
 function App() {
-  return <Timer />;
+  const [width, setWidth] = useState(0);
+  const buttonRef = useRef(null);
+
+  useLayoutEffect(() => {
+    const infos = buttonRef.current.getBoundingClientRect();
+    setWidth(infos.width);
+  }, []);
+
+  return (
+    <>
+      <button ref={buttonRef}>Je suis un bouton</button>
+      <p>{Math.floor(width)}</p>
+    </>
+  );
 }
 
 export default App;
