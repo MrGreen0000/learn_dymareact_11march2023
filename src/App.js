bba4147f825d02b35bfb00ff0593def84e506afb
Naji Mouflih <91195806+MrGreen0000@users.noreply.github.com>
2024-01-06 19:04:07 +0100

    mise en cache d'uncomposant avec useMemo()

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import { useState } from "react";
-import Calendar from "./Calendar";
+import { useMemo, useState } from "react";
 
 function App() {
   console.log("render App");
-  const [value, setValue] = useState("");
-  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(0);
+  const [list, setList] = useState([]);
+
+  function complexeCalculayion(list) {
+    console.log("complexe function invocation");
+    return list.map((l) => {
+      for (let i = 0; i < 100000; i++) {
+        l = i;
+      }
+      return l;
+    });
+  }
+
+  const complexeList = useMemo(() => {
+    console.time("computation");
+    const res = complexeCalculayion(list);
+    console.timeEnd("computation");
+    return res;
+  }, [list]);
 
   return (
     <>
       <input
-        type="text"
+        type="number"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
-      <p>{value}</p>
-      <button onClick={(e) => setCount(count + 1)}>+1</button>
-      <Calendar count={count} />
+
+      <button
+        onClick={() => {
+          setList([...list, value]);
+          setValue(0);
+        }}
+      >
+        Add to list
+      </button>
+      <ul>
+        {complexeList.map((cl, i) => (
+          <li key={i}>{cl}</li>
+        ))}
+      </ul>
     </>
   );
 }
--- src/Calendar.js
+++
@@ -1,8 +0,0 @@
-import { memo } from "react";
-
-function Calendar({ count }) {
-  console.log("render Calendar");
-  return <h1>Calendar {count}</h1>;
-}
-
-export default memo(Calendar, (prevProp, nextProp) => {});
